39c3eb98369ec6582021eb46c13712f597414d67
Mikhail Martusevich <61042574+psxzz@users.noreply.github.com>
2021-05-13 21:53:35 +0300

    Add files via upload

--- udp-server/src/UDP_Server.java
+++ udp-server/src/UDP_Server.java
@@ -1,38 +1,140 @@
 import java.net.*;
 import java.io.*;
-
+import java.util.ArrayList;
 
 public class UDP_Server extends Thread  {
 
-    public final static int PORT = 8888;        // ServerSocket port
-    private DatagramSocket socket;              // UDP socket
-    private boolean running;                    // server status variable
-    private byte[] recvBuffer = new byte[256];  // receiving messages buffer
-    private byte[] sendBuffer = new byte[256];  // sending messages buffer
+    private static class SERVER_COMMANDS {  // structure of server commands
+        final String SERVER_END = "/end";
+        final String REG_USER = "/reg";
+        final String LOG_USER = "/login";
+        final String LOGOUT_USER = "/logout";
+    }
+
+    private static class SERVER_USER {  // structure of server clients
+        String userName;
+        InetAddress userAddress;
+        boolean isOnline = false;
+
+        public SERVER_USER(String _name, InetAddress _inetaddr) {
+            userName = _name;
+            userAddress = _inetaddr;
+            isOnline = false;
+        }
+    }
+
+    public final static int         SERVER_PORT = 8888;         // ServerSocket port
+    private final DatagramSocket    SERVER_SOCKET;              // UDP socket
+    private boolean isRunning;                                  // server status variable
+
+    private byte[]  RECEIVE_BUFFER  = new byte[256];  // receiving messages buffer
+    //private byte[]  SEND_BUFFER     = new byte[256];  // sending messages buffer
+
 
     public UDP_Server() throws IOException {
-        socket = new DatagramSocket(PORT);
+        SERVER_SOCKET = new DatagramSocket(SERVER_PORT);
     }
 
     public void Run() throws IOException {
-        running = true;
+        SERVER_COMMANDS commands = new SERVER_COMMANDS();
+        ArrayList<SERVER_USER> users = new ArrayList<SERVER_USER>();
+
+        isRunning = true;
         System.out.println("Server started");
 
-        while (running) {
-            DatagramPacket recvPacket = new DatagramPacket(recvBuffer, recvBuffer.length); // UDP receiving packet
+
+        while (isRunning) {
+            DatagramPacket RECEIVED_PACKET = new DatagramPacket(RECEIVE_BUFFER, RECEIVE_BUFFER.length); // UDP receiving packet
 
             System.out.println("Waiting for a client message... ");
-            socket.receive(recvPacket); // receive data from client
+            SERVER_SOCKET.receive(RECEIVED_PACKET); // receive data from client
 
-            String recvData = new String(recvPacket.getData()); // convert recieved data to string
-            System.out.println("Received from client: " + recvData);
+            String RECEIVED_DATA = new String(RECEIVED_PACKET.getData(), 0, RECEIVED_PACKET.getLength()); // convert received data to string
+//            System.out.println("Received from client: " + RECEIVED_DATA);
 
-            if (recvData.equals("end")) {
-                System.out.println("Server ended");
-                running = false;
+
+            /* Server received registration command (/reg)*/
+
+            if (RECEIVED_DATA.equals(commands.REG_USER)) { // if received register command
+                RECEIVE_BUFFER = new byte[256]; //clearing for a new message
+                boolean exist = false;
+                InetAddress regAddress = RECEIVED_PACKET.getAddress(); // save address of client
+
+                do {
+                    SERVER_SOCKET.receive(RECEIVED_PACKET);
+                } while (!RECEIVED_PACKET.getAddress().equals(regAddress)); // waiting to receive username
+
+                String regUser = new String(RECEIVED_PACKET.getData(), 0, RECEIVED_PACKET.getLength());
+
+                for (int i = 0; i < users.size(); i++)
+                    if (regUser.equals(users.get(i).userName)) { // check all users for equal username
+                        exist = true;
+                        // send error to client
+                        break;
+                    }
+
+                if (!exist) {
+                    users.add(new SERVER_USER(regUser,RECEIVED_PACKET.getAddress())); // adding new client to userlist
+                    // send registration success to client
+                    System.out.println("New user registered: " + regUser);
+                }
             }
+
+
+            /* Server received login command (/login) */
+
+            else if (RECEIVED_DATA.equals(commands.LOG_USER)) {
+                RECEIVE_BUFFER = new byte[256]; //clearing for a new message
+                boolean exist = false;
+                InetAddress loginAddress = RECEIVED_PACKET.getAddress();
+
+                do {
+                    SERVER_SOCKET.receive(RECEIVED_PACKET);
+                } while (!RECEIVED_PACKET.getAddress().equals(loginAddress)); // waiting to receive username
+
+
+                String loginUser = new String(RECEIVED_PACKET.getData(), 0, RECEIVED_PACKET.getLength());
+
+                for (int i = 0; i < users.size(); i++) {
+                    if (loginUser.equals(users.get(i).userName) && !users.get(i).isOnline) // check all users for equal username
+                        exist = true;
+
+                    if (exist) {
+                        users.get(i).userAddress = RECEIVED_PACKET.getAddress();
+                        users.get(i).isOnline = true;
+                        // send login success to client
+                        System.out.println("User logged in: " + loginUser);
+                        break;
+                    }
+                }
+
+                if (!exist) {
+                    // send error to client: user is online
+                }
+            }
+
+            /* Server received user logout command (/logout) */
+
+            else if (RECEIVED_DATA.equals(commands.LOGOUT_USER)) {
+                continue;
+            }
+
+            /* Server received end of work command (/end) */
+            else if (RECEIVED_DATA.equals(commands.SERVER_END)) {
+                System.out.println("Server ended");
+                // send to all users ending message
+                isRunning = false;
+            }
+
+            /* Server received user's message */
+            else {
+                System.out.println("Received from client: " + RECEIVED_DATA);
+                // send message to all users
+            }
+
+            RECEIVE_BUFFER = new byte[256]; //clearing for a new message
         }
-        socket.close();
+        SERVER_SOCKET.close();
     }
 
     public static void main(String[] args) throws IOException {
